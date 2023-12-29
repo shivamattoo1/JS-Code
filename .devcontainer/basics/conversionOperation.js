162327461e3d4f6b0b65e5b7cbaf52b31928e471
shivamattoo1 <154424795+shivamattoo1@users.noreply.github.com>
2023-12-29 11:13:03 +0000

    learning conversion

--- .devcontainer/basics/conversionOperation.js
+++ .devcontainer/basics/conversionOperation.js
@@ -5,3 +5,17 @@ console.log(typeof score)
 let valueInNumber= Number(score);
 
 console.log(typeof valueInNumber)
+const heros=["a","b","c"]
+
+let myObj={
+    name:"Shiva",
+    age:"30",
+}
+console.log(myObj)
+
+const myFunction=function(){
+    console.log("Hello World")
+
+}
+myFunction();
+console.log(myFunction)
