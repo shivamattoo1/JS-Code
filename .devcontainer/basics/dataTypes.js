df7d58b5d0d7f33635b61b29fe2ba0e63ba1e024
shivamattoo1 <154424795+shivamattoo1@users.noreply.github.com>
2023-12-29 10:52:47 +0000

    datatypes in js

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";
+//treat all js code as newer js
+
+//alert(3+3) we are using nodejs not browser
+
+let name="Shiva"
+let age=19
+let isLoggedIn= false;
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof name);
+console.log(typeof age);
